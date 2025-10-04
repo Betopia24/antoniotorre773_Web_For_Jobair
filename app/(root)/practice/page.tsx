fd30be7b9580e900d35a79a8702a14e18cc89333
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-04 09:28:45 +0600

    UI: Practice Page

---
+++ app/(root)/practice/page.tsx
@@ -0,0 +1,12 @@
+import PracticeHero from "@/components/practice/PracticeHero";
+import Tasks from "@/components/practice/Tasks";
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <PracticeHero />
+      <Tasks />
+    </>
+  );
+}
