430e641b6e9310ff7a98121127d21fc8fdd7fd92
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-19 15:38:50 +0600

    Add not-found page handler for unmatched routes
    Co-authored-by: Rayan Rafi <rayanrafi050@gmail.com>

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='flex min-h-screen items-center justify-center font-extrabold'>
+      <h1 className="text-5xl text-red-500">404 - Not Found</h1>
+    </div>
+  )
+}
