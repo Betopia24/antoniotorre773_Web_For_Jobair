d1fa815b79ca91eefc32560e7e9811948a1466da
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-25 10:12:21 +0600

    Translator: Global Wrapper

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,10 +8,12 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <GoogleTranslateWrapper>
-      <Navbar/>
+    <>
+      {/* <GoogleTranslateWrapper> */}
+      <Navbar />
       <main>{children}</main>
-      <Footer/>
-    </GoogleTranslateWrapper>
+      <Footer />
+      {/* </GoogleTranslateWrapper> */}
+    </>
   );
 }
